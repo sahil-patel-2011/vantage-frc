dde1e103e85a621b44b8f62dfc916a5f8cac5bae
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 16:17:44 -0700

    Add Lovat teleop last 24 matches field zone occupancy. (#799)

---
+++ apps/web/app/lovat-kit/teleop-last24-zone/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · last 24 matches",
+};
+
+export default function TeleopLast24ZonePage() {
+  return <LovatKitPageClient />;
+}
