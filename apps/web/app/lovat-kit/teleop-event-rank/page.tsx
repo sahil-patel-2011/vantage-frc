3e0df5d4ea12c5765fbe14820be350e7cc2a3386
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 15:20:47 -0700

    Add Lovat teleop this event dense rank. (#580)

---
+++ apps/web/app/lovat-kit/teleop-event-rank/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · this event",
+};
+
+export default function TeleopEventRankPage() {
+  return <LovatKitPageClient />;
+}
