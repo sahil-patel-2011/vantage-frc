9c048dc2d6e4e256c21ddbf0b95aa7ea893f11a1
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 16:44:35 -0700

    Add Lovat endgame last 3 matches path geometry. (#926)

---
+++ apps/web/app/lovat-kit/endgame-recent3-path/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · last 3 matches",
+};
+
+export default function EndgameRecent3PathPage() {
+  return <LovatKitPageClient />;
+}
