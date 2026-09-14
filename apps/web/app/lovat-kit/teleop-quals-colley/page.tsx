cf81d5d3b85e6dbd6cc6919cbf75fb3357e97d38
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 16:10:40 -0700

    Add Lovat teleop qualification matches colley ranking. (#768)

---
+++ apps/web/app/lovat-kit/teleop-quals-colley/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · qualification matches",
+};
+
+export default function TeleopQualsColleyPage() {
+  return <LovatKitPageClient />;
+}
