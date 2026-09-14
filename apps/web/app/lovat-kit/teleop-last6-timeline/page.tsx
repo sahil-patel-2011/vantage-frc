4cee4260809c611140f44a88bf6b83afa71d515e
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 15:59:16 -0700

    Add Lovat teleop last 6 matches timed occupancy. (#719)

---
+++ apps/web/app/lovat-kit/teleop-last6-timeline/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · last 6 matches",
+};
+
+export default function TeleopLast6TimelinePage() {
+  return <LovatKitPageClient />;
+}
