89bfd2bdaac34564d0320ff64f5e06dc2a9532c5
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 17:15:50 -0700

    Add Lovat teleop this event lead lookup notes. (#1037)

---
+++ apps/web/app/lovat-kit/teleop-event-notes/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · this event",
+};
+
+export default function TeleopEventNotesPage() {
+  return <LovatKitPageClient />;
+}
