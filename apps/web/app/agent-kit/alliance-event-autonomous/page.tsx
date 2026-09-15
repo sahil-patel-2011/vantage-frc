cabd1374a3e27ec4b45d2f6eb655a7317b447d71
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-15 04:50:53 -0700

    Add agent alliance this event autonomous loop. (#2143)

---
+++ apps/web/app/agent-kit/alliance-event-autonomous/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Alliance · this event",
+};
+
+export default function AllianceEventAutonomousPage() {
+  return <LovatKitPageClient />;
+}
