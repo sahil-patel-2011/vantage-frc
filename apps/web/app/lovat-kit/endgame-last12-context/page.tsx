de418ae81c551da132a38fedb9fa084d514666cc
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:40:03 -0400

    Add Lovat endgame last 12 matches context-visible scouting.

---
+++ apps/web/app/lovat-kit/endgame-last12-context/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · last 12 matches",
+};
+
+export default function EndgameLast12ContextPage() {
+  return <LovatKitPageClient />;
+}
