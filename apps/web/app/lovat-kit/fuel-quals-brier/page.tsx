40ba34b5d58d2cf56a12894c11f5074dbb98fb93
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 19:35:13 -0700

    Add Lovat fuel qualification matches forecast calibration. (#1587)

---
+++ apps/web/app/lovat-kit/fuel-quals-brier/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · qualification matches",
+};
+
+export default function FuelQualsBrierPage() {
+  return <LovatKitPageClient />;
+}
