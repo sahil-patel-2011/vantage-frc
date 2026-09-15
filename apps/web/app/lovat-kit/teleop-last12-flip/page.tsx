ed00a113756cd09c9edbb0ca2d6014cb9d3d6e24
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:20:22 -0400

    Add Lovat teleop last 12 matches red-blue alliance flip.

---
+++ apps/web/app/lovat-kit/teleop-last12-flip/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · last 12 matches",
+};
+
+export default function TeleopLast12FlipPage() {
+  return <LovatKitPageClient />;
+}
