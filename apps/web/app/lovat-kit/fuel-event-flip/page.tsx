9a5c6b3adebb29cf9e3b0933a82a864a840d9479
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 19:17:01 -0700

    Add Lovat fuel this event red-blue alliance flip. (#1507)

---
+++ apps/web/app/lovat-kit/fuel-event-flip/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · this event",
+};
+
+export default function FuelEventFlipPage() {
+  return <LovatKitPageClient />;
+}
