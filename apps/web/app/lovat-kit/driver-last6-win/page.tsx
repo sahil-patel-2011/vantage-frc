d92ebda289676c8ec0498d539819552e168c4672
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:25:10 -0700

    Add Lovat driver last 6 matches match win probability. (#1773)

---
+++ apps/web/app/lovat-kit/driver-last6-win/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · last 6 matches",
+};
+
+export default function DriverLast6WinPage() {
+  return <LovatKitPageClient />;
+}
