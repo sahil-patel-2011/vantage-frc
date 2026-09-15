cd74bfe02e274769df76a0957cd99de7da48d00a
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:25:22 -0700

    Add Lovat defense last 12 matches field-shrunk. (#1311)

---
+++ apps/web/app/lovat-kit/defense-last12-shrink/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · last 12 matches",
+};
+
+export default function DefenseLast12ShrinkPage() {
+  return <LovatKitPageClient />;
+}
