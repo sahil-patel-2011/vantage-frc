176f6fb53741bffb08b387aa1675ca1609b0870a
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 19:07:50 -0700

    Add Lovat defense playoff matches 22-metric lookup. (#1472)

---
+++ apps/web/app/lovat-kit/defense-playoffs-lookup/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · playoff matches",
+};
+
+export default function DefensePlayoffsLookupPage() {
+  return <LovatKitPageClient />;
+}
