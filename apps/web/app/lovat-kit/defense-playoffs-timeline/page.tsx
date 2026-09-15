60fbfb222b1234303cfa850935536ca3625180e6
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 19:04:09 -0700

    Add Lovat defense playoff matches timed occupancy. (#1460)

---
+++ apps/web/app/lovat-kit/defense-playoffs-timeline/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · playoff matches",
+};
+
+export default function DefensePlayoffsTimelinePage() {
+  return <LovatKitPageClient />;
+}
