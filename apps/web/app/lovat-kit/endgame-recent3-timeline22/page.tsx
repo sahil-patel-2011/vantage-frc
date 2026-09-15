f3cfed37fef78043a694cfc6c9dd8bb1215120ff
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 17:45:47 -0700

    Add Lovat endgame last 3 matches 22-metric timeline. (#1162)

---
+++ apps/web/app/lovat-kit/endgame-recent3-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · last 3 matches",
+};
+
+export default function EndgameRecent3Timeline22Page() {
+  return <LovatKitPageClient />;
+}
