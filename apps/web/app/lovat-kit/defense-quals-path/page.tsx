ff72570f82abf76d9b66e61bed65becbfc30a444
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:32:35 -0700

    Add Lovat defense qualification matches path geometry. (#1336)

---
+++ apps/web/app/lovat-kit/defense-quals-path/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · qualification matches",
+};
+
+export default function DefenseQualsPathPage() {
+  return <LovatKitPageClient />;
+}
