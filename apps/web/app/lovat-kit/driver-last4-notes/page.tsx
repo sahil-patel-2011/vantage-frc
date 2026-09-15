b87a24633d02e8c240bb97a168632b50d484a263
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 21:05:13 -0700

    Add Lovat driver last 4 matches lead lookup notes. (#1925)

---
+++ apps/web/app/lovat-kit/driver-last4-notes/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · last 4 matches",
+};
+
+export default function DriverLast4NotesPage() {
+  return <LovatKitPageClient />;
+}
