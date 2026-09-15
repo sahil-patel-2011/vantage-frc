3550d986bf07a3441c17d221896f1c03b4ed721d
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 21:24:13 -0400

    Add Lovat defense last 12 matches path interference.

---
+++ apps/web/app/lovat-kit/defense-last12-interfere/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · last 12 matches",
+};
+
+export default function DefenseLast12InterferePage() {
+  return <LovatKitPageClient />;
+}
