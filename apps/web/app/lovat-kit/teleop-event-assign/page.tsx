07e7cac0bbaabcaac70f9d317be0ad3ed3e8b11a
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:16:26 -0400

    Add Lovat teleop this event assignment range.

---
+++ apps/web/app/lovat-kit/teleop-event-assign/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · this event",
+};
+
+export default function TeleopEventAssignPage() {
+  return <LovatKitPageClient />;
+}
