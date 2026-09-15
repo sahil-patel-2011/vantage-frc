1e9e6f79393d9660b499906c8b061be2cf634329
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:43:46 -0700

    Add Lovat defense last 3 matches forecast calibration. (#1376)

---
+++ apps/web/app/lovat-kit/defense-recent3-brier/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · last 3 matches",
+};
+
+export default function DefenseRecent3BrierPage() {
+  return <LovatKitPageClient />;
+}
