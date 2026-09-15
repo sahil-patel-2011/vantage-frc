bf6bc79b3dcda3c08753214faa64b9f7ec543f77
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 23:13:41 -0400

    Add Lovat driver this event field-shrunk.

---
+++ apps/web/app/lovat-kit/driver-event-shrink/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · this event",
+};
+
+export default function DriverEventShrinkPage() {
+  return <LovatKitPageClient />;
+}
