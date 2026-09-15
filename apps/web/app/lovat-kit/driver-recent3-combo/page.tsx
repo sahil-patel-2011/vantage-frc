f4058c581816430bb19e59c846f20f32a79797e8
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:46:10 -0700

    Add Lovat driver last 3 matches alliance combination. (#1848)

---
+++ apps/web/app/lovat-kit/driver-recent3-combo/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · last 3 matches",
+};
+
+export default function DriverRecent3ComboPage() {
+  return <LovatKitPageClient />;
+}
