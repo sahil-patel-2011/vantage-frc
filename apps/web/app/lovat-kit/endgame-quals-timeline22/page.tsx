1201a10862d3be7a2dd7f9575cce75e052231bb1
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:43:24 -0400

    Add Lovat endgame qualification matches 22-metric timeline.

---
+++ apps/web/app/lovat-kit/endgame-quals-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · qualification matches",
+};
+
+export default function EndgameQualsTimeline22Page() {
+  return <LovatKitPageClient />;
+}
