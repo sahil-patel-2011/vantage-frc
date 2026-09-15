16fb4ce382e7d2eabbbb4c11ed559f4961f24f3d
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:27:47 -0700

    Add Lovat driver last 12 matches consistency. (#1784)

---
+++ apps/web/app/lovat-kit/driver-last12-consistency/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · last 12 matches",
+};
+
+export default function DriverLast12ConsistencyPage() {
+  return <LovatKitPageClient />;
+}
