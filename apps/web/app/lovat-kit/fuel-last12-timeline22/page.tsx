e511cd86783bd455c5016ffc0cb3de85b219effb
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 22:31:30 -0400

    Add Lovat fuel last 12 matches 22-metric timeline.

---
+++ apps/web/app/lovat-kit/fuel-last12-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · last 12 matches",
+};
+
+export default function FuelLast12Timeline22Page() {
+  return <LovatKitPageClient />;
+}
