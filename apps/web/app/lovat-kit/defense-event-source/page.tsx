32e338574232ec95693481cc107534d4f6d66ab1
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:13:54 -0700

    Add Lovat defense this event event-scout mix. (#1269)

---
+++ apps/web/app/lovat-kit/defense-event-source/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · this event",
+};
+
+export default function DefenseEventSourcePage() {
+  return <LovatKitPageClient />;
+}
