5af5eb9244ca607824faf9fb28bd7722ed59e03d
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 21:05:43 -0400

    Add Lovat endgame playoff matches 22-metric timeline.

---
+++ apps/web/app/lovat-kit/endgame-playoffs-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · playoff matches",
+};
+
+export default function EndgamePlayoffsTimeline22Page() {
+  return <LovatKitPageClient />;
+}
