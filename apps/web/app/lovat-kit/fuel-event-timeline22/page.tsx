a21042111dff2c9a7dde9a73ba6030237775b38a
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 22:17:52 -0400

    Add Lovat fuel this event 22-metric timeline.

---
+++ apps/web/app/lovat-kit/fuel-event-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · this event",
+};
+
+export default function FuelEventTimeline22Page() {
+  return <LovatKitPageClient />;
+}
