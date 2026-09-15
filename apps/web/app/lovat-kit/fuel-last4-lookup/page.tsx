5878fb8166fe7766e14d176cfbfc6871f99392f6
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 23:01:16 -0400

    Add Lovat fuel last 4 matches 22-metric lookup.

---
+++ apps/web/app/lovat-kit/fuel-last4-lookup/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · last 4 matches",
+};
+
+export default function FuelLast4LookupPage() {
+  return <LovatKitPageClient />;
+}
