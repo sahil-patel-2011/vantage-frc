1176f355ff062d817375a6e498ff5a99ddfc0991
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:38:26 -0700

    Add Lovat defense qualification matches assignment range. (#1358)

---
+++ apps/web/app/lovat-kit/defense-quals-assign/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · qualification matches",
+};
+
+export default function DefenseQualsAssignPage() {
+  return <LovatKitPageClient />;
+}
