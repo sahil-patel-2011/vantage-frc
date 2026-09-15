8b2899cf658e0119631d10218afcaac359c87235
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 21:14:38 -0700

    Add Lovat driver playoff matches event-scout mix. (#1959)

---
+++ apps/web/app/lovat-kit/driver-playoffs-source/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Driver · playoff matches",
+};
+
+export default function DriverPlayoffsSourcePage() {
+  return <LovatKitPageClient />;
+}
