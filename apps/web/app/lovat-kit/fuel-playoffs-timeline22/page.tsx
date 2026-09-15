5e8bb47fd0034ba87fa3433afabeb9d80204a64c
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 23:11:03 -0400

    Add Lovat fuel playoff matches 22-metric timeline.

---
+++ apps/web/app/lovat-kit/fuel-playoffs-timeline22/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Fuel · playoff matches",
+};
+
+export default function FuelPlayoffsTimeline22Page() {
+  return <LovatKitPageClient />;
+}
