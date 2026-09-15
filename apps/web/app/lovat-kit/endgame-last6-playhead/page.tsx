c28d6e0c455415bd6e7a7dbe11fd7760fab2772a
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 17:37:02 -0700

    Add Lovat endgame last 6 matches auto path playhead. (#1126)

---
+++ apps/web/app/lovat-kit/endgame-last6-playhead/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Endgame · last 6 matches",
+};
+
+export default function EndgameLast6PlayheadPage() {
+  return <LovatKitPageClient />;
+}
