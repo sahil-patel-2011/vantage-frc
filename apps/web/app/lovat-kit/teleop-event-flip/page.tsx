2a6536ee6c0d92091330ab5b038eb029e908fecb
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 20:15:58 -0400

    Add Lovat teleop this event red-blue alliance flip.

---
+++ apps/web/app/lovat-kit/teleop-event-flip/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Teleop · this event",
+};
+
+export default function TeleopEventFlipPage() {
+  return <LovatKitPageClient />;
+}
