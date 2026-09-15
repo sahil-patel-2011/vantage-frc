6032a1fcc082c0c46742ed73bc69687b59bde910
Sahil Patel <sahiljpatel2011@gmail.com>
2026-09-14 18:11:37 -0700

    Add Lovat defense this event match win probability. (#1263)

---
+++ apps/web/app/lovat-kit/defense-event-win/page.tsx
@@ -0,0 +1,9 @@
+import LovatKitPageClient from "./board";
+
+export const metadata = {
+  title: "Defense · this event",
+};
+
+export default function DefenseEventWinPage() {
+  return <LovatKitPageClient />;
+}
